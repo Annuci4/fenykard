94b850e79a3f4ab8290d02720c574b36d158bab6
Annuci4 <36335608+Annuci4@users.noreply.github.com>
2018-02-12 19:55:20 +0100

    Update withoutif.cpp

--- withoutif.cpp
+++ withoutif.cpp
@@ -5,7 +5,7 @@
 int
 main (void)
 {
-    int xj = 0, xk = 0, yj = 0, yk = 0;
+    int xj = 1, xk = 1, yj = 1, yk = 1;
     int mx = 80 * 2, my = 24 * 2;
 
     WINDOW *ablak;
@@ -13,7 +13,7 @@ main (void)
     noecho ();
     cbreak ();
     nodelay (ablak, true);
-
+    
     for (;;)
     {
         xj = (xj - 1) % mx;
@@ -23,13 +23,20 @@ main (void)
         yk = (yk + 1) % my;
 
         clear ();
-
-        mvprintw (0, 0,
-                  "--------------------------------------------------------------------------------");
-        mvprintw (24, 0,
-                  "--------------------------------------------------------------------------------");
+        int i;
+        for(i=1;i<=my;i++)
+            {
+			mvprintw (i, 0 , ".");
+			mvprintw (i, 80, ".");
+		    }
+        int j;
+         for(j=1;i<=mx;j++)
+            {
+			mvprintw (0, j , ".");
+			mvprintw (24, j, ".");
+		    }
         mvprintw (abs ((yj + (my - yk)) / 2),
-                  abs ((xj + (mx - xk)) / 2), "X");
+                  abs ((xj + (mx - xk)) / 2), "o");
 
         refresh ();
         usleep (150000);
