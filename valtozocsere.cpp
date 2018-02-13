500e6a9a3504550e9e4c3e206cd07167ee8eef8b
Annuci4 <36335608+Annuci4@users.noreply.github.com>
2018-02-13 20:46:33 +0100

    Update valtozocsere.cpp

--- valtozocsere.cpp
+++ valtozocsere.cpp
@@ -7,14 +7,19 @@ int main()
     int b = 42;
 
     cout<<"kezdo: "<<"a="<<a<<" b="<<b<<endl;
+    
     a = b - a;
     b = b - a;
     a = b + a;
+    
     cout<<"1. csere osszeadassal es kivonassal: "<<"a="<<a<<" b="<<b<<endl;
+    
     a = b * a;
     b = a / b;
     a = a / b;
+    
     cout<<"2. csere szorzassal es osztassal: "<<"a="<<a<<" b="<<b<<endl;
+    
     return 0;
 }
 
